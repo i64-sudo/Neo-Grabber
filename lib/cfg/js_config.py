378d80b747e0b0aef85e27d42a6f5c6ee4a06a36
wk <32526701+i64-sudo@users.noreply.github.com>
2023-05-18 20:43:31 -0700

    Update js_config.py

--- lib/cfg/js_config.py
+++ lib/cfg/js_config.py
@@ -2,7 +2,7 @@
 class js_bot:
     js_avatar_url = "https://ae01.alicdn.com/kf/A4e4fc27528524fa3a382396ad8ce7f1af/Dakimakura-funda-de-almohada-corporal-orc-para-mujer-estampado-de-doble-cara-tama-o-real-Anime.png"
     js_webhook_name = "Neo Hook (femboy furry UwU)"
-    js_webhook = "https://discord.com/api/webhooks/1107616492666757161/KHJ3mYEG7DFC3NN6ibO1rK3atYSdyCVAsRCWJ2r7XrCv5CXB5xLyMnC-yifN6mqvUwV3"
+    js_webhook = ""
 
 class __config__:
     __runonce__ = "y"
